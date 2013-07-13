ad6afbf918f1b8efa4836412259d904fd0cadbdb
ebinion <ezekiel@gridshiftdesign.com>
2013-07-13 15:38:09 -0500

    Rearranging directory structure to be appropriate for app release

--- config.rb
+++ config.rb
@@ -1,12 +1,12 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
+http_path = "app/"
+css_dir = "app/css"
 sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
-fonts_dir = "fonts"
+images_dir = "app/img"
+javascripts_dir = "app/js"
+fonts_dir = "app/fonts"
 
 output_style = :nested
 
